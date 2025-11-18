47048e350c10ce029624424d43d01e33bbc6d39b
ModitTdr <modittuladhar@gmail.com>
2025-11-18 16:26:25 +0545

    update: projects-updatedd

--- data/projects.ts
+++ data/projects.ts
@@ -1,8 +1,8 @@
 export const projects = [
   {
-    link: "#",
+    link: "https://github.com/ModitTdr/chefMandu",
     title: "Chefmandu",
-    description: "Chefmandu description goes here",
+    description: "Chefmandu is a recipe management system that allows you to store and manage your favorite recipes.",
     logo: "/chefMandu(logo).svg",
     background: "bg-orange-300",
     textColor: "black",
@@ -15,7 +15,7 @@ export const projects = [
   {
     link: "https://roomfinder.modittuladhar.com.np/",
     title: "RoomFinder",
-    description: "Roomfinder description goes here",
+    description: "Roomfinder is a online platform to find your desired room in one place with a user-friendly interface and smart recommendations based on your preferences.",
     logo: "",
     background: "bg-gradient-to-tr from-orange-600/90 from-5% via-orange-400 via-100%",
     textColor: "black",
@@ -28,7 +28,7 @@ export const projects = [
   {
     link: "https://pokementor.vercel.app/",
     title: "Pokementor",
-    description: "Pokementor description goes here",
+    description: "Pokementor is an online tool to help your pokemon reach it's full potential. ",
     logo: "",
     background: "bg-gradient-to-b from-neutral-950 from-5% via-neutral-900 via-100%",
     textColor: "neutral-100",
