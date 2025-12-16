d9c245b70d15983013d1c269cb855bb199dc04f0
ModitTdr <modittuladhar@gmail.com>
2025-12-16 13:32:14 +0545

    update: project-update

--- data/projects.ts
+++ data/projects.ts
@@ -15,7 +15,7 @@ export const projects = [
   {
     link: "https://roomfinder.modittuladhar.com.np/",
     title: "RoomFinder",
-    description: "Roomfinder is a online platform to find your desired room in one place with a user-friendly interface and smart recommendations based on your preferences.",
+    description: "Roomfinder is an online platform to find your desired room in one place with a user-friendly interface and smart recommendations based on your preferences.",
     logo: "",
     background: "bg-gradient-to-tr from-orange-600/90 from-5% via-orange-400 via-100%",
     textColor: "black",
@@ -31,6 +31,19 @@ export const projects = [
     description: "Pokementor is an online tool to help your pokemon reach it's full potential. ",
     logo: "",
     background: "bg-gradient-to-b from-neutral-950 from-5% via-neutral-900 via-100%",
+    textColor: "neutral-100",
+    technologies: [
+      { name: "nextjs", src: "/javascript.svg" },
+      { name: "typescript", src: "/tailwindcss.svg" },
+      { name: "tanstack", src: "/html5.svg" },
+    ]
+  },
+  {
+    link: "https://uihaven-design.vercel.app/",
+    title: "Uihaven",
+    description: "Uihaven is a place to start planning your next design. It provides simple tools to pair fonts and create moodboards.",
+    logo: "",
+    background: "bg-gradient-to-b from-indigo-600 from-5% via-blue-500 via-100%",
     textColor: "neutral-100",
     technologies: [
       { name: "nextjs", src: "/javascript.svg" },
